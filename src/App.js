58a10653a04191793e2f9658a812a59fe984f738
3sang <1285480439@qq.com>
2021-05-21 21:46:17 +0800

    react项目修改

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/*
+ * Description  : 首页
+ * Author       : Saraku.
+ * Date         : 2021-05-21 20:16:44
+ * LastEditors  : Saraku.
+ * LastEditTime : 2021-05-21 20:18:26
+ */
+
+import React from "react";
+import { Button } from "antd";
+
+function App() {
+  return <Button>提交</Button>;
+}
+
+export default App;
